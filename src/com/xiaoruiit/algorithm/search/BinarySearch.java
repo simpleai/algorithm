8340f3e4ed2914e9c2674b6e2847c4c5f11c4e66
simpleai <simpleintelligence@qq.com>
2021-08-03 06:59:52 +0800

    二分查找递归写法

--- src/com/xiaoruiit/algorithm/search/BinarySearch.java
+++ src/com/xiaoruiit/algorithm/search/BinarySearch.java
@@ -5,6 +5,28 @@ package com.xiaoruiit.algorithm.search;
  */
 public class BinarySearch {
 
+    public static void main(String[] args) {
+
+        // 二分查找
+        int [] arr = {1,2,3,4,5,6};
+        System.out.println(BinarySearch.binary(arr, 5));
+        // 二分查找递归
+        int [] arr2 = {1,2,3,4,5,6};
+        System.out.println(BinarySearch.binaryRecursion(arr, 5));
+
+
+        // 有序数组中，第一个大于data的数组下标
+        int [] arr3 = {1,2,3,4,5,6,7,8,9};
+        System.out.println(BinarySearch.greaterThan(arr3, 5));
+    }
+
+    /**
+     * 非递归写法
+     *
+     * @param arr
+     * @param data
+     * @return
+     */
     public static boolean binary(int[] arr, int data){
         if (arr == null || arr.length < 1){
             return false;
@@ -29,10 +51,43 @@ public class BinarySearch {
     }
 
     /**
+     * 递归写法
+     *
+     * @param arr
+     * @param target
+     * @return
+     */
+    public static int  binaryRecursion(int[] arr, int target){
+
+        int low = 0;
+        int high = arr.length-1;
+        return binaryRecursionExec(arr, target, low, high);
+    }
+
+    private static int binaryRecursionExec(int[] arr, int target, int low, int high) {
+        if (low > high){
+            return -1;
+        }
+
+        int middle = low + (high-low) /2 ;
+
+        if (arr[middle] == target){
+            return middle;
+        } else if (arr[middle] > target){
+            return binaryRecursionExec(arr, target, low, middle - 1);
+        } else {
+            return binaryRecursionExec(arr, target, middle + 1, high);
+        }
+    }
+
+    /**
      * 求有序数组中，第一个大于data的数组下标
+     *
      * @param arr 有序数组
      * @param data
      * @return
+     *
+     * 分析：有序数组第一个大于data，即二分中间的数大于data,中间的前一个数小于data.
      */
     public static int greaterThan(int[] arr, int data){
         if (arr == null || arr.length < 1){
