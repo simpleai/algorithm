604422885607e001a7725f96d537615a2fed43ac
simpleai <simpleintelligence@qq.com>
2021-08-16 07:15:39 +0800

    调整

--- src/com/xiaoruiit/algorithm_problem/AlgorithmProblem.java
+++ src/com/xiaoruiit/algorithm_problem/AlgorithmProblem.java
@@ -376,8 +376,15 @@ public class AlgorithmProblem {
      * 单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
      *
      */
-    public static List<String> leetCode212(String[][] board, List<String> words){
+    public static List<String> leetCode212(char[][] board, String[] words){
+        List<String> result = new ArrayList<>();
+        // 校验
 
+        // 变量
+
+
+
+        return result;
     }
 
     public static void main(String[] args) {
