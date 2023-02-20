35d5aa26a992f6b57ab7b8849afcd6f76a8bfd24
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2023-02-20 09:01:23 +0800

    feat: 链表

--- src/com/xiaoruiit/data_structure/LinkedList/LinkedListProblem.java
+++ src/com/xiaoruiit/data_structure/LinkedList/LinkedListProblem.java
@@ -3,6 +3,8 @@ package com.xiaoruiit.data_structure.LinkedList;
 import com.xiaoruiit.data_structure.stack.StackProblem;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * 链表算法题
@@ -35,6 +37,43 @@ public class LinkedListProblem {
         System.out.print("25.K个一组翻转链表2：");
         MyLinkedList.print(LinkedListProblem.reverseKGroup(new MyLinkedList().init(),3));
 
+    }
+
+    /**
+     * LeetCode 困难 23.合并K个升序链表
+     * 思路：使用优先队列
+     */
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null){
+            return null;
+        }
+
+        ListNode hummy = new ListNode();
+        ListNode current = hummy;
+        PriorityQueue<ListNode> priorityQueue = new PriorityQueue(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+        // 初始化小根堆
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null)
+                priorityQueue.offer(lists[i]);
+        }
+
+        while (!priorityQueue.isEmpty()){
+            ListNode poll = priorityQueue.poll();
+
+            current.next = poll;// 连接
+            current = current.next;// 前移
+
+            if (poll.next != null){
+                priorityQueue.offer(poll.next);
+            }
+        }
+
+        return hummy.next;
     }
 
     /**
