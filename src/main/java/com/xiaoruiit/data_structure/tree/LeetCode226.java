889c7de0e530f548a5eca189e93f2b956a2746c5
hanxiaorui <543843594@qq.com>
2024-04-15 08:55:43 +0800

    feat: leet

---
+++ src/main/java/com/xiaoruiit/data_structure/tree/LeetCode226.java
@@ -0,0 +1,50 @@
+package com.xiaoruiit.data_structure.tree;
+
+import com.xiaoruiit.util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 翻转二叉树
+ */
+public class LeetCode226 {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(2);
+        list.add(7);
+        list.add(1);
+        list.add(3);
+        list.add(6);
+        list.add(9);
+        TreeNode treeNode = TreeNode.constructTree(list);
+        TreeNode.traverseTree(treeNode);
+        System.out.println();
+        invertTree(treeNode);
+        TreeNode.traverseTree(treeNode);
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+
+        re(root);
+
+        return root;
+    }
+
+    private static void re(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left != null || root.right != null) {
+            TreeNode t = root.left;
+            root.left = root.right;
+            root.right = t;
+        }
+
+        re(root.left);
+        re(root.right);
+    }
+}
