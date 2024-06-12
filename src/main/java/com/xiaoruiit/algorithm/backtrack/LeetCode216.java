10a814da92828be4fcfba4499fd813d6942a2d31
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2024-06-12 20:40:52 +0800

    feat: leet

--- src/main/java/com/xiaoruiit/algorithm/backtrack/LeetCode216.java
+++ src/main/java/com/xiaoruiit/algorithm/backtrack/LeetCode216.java
@@ -14,14 +14,21 @@ public class LeetCode216 {
     public static void main(String[] args) {
         LeetCode216 leet = new LeetCode216();
         System.out.println(leet.combinationSum3(3, 7));// 三个1-9的数和为7
+        leet.res = new ArrayList<>();
         System.out.println(leet.combinationSum3(3, 9));
+        leet.res = new ArrayList<>();
         System.out.println(leet.combinationSum3(4, 1));
 
         LeetCode216 leet2 = new LeetCode216();
         leet2.res = new ArrayList<>();
-        System.out.println(leet2.combinationSum3(3, 7));// 三个1-9的数和为7
-        System.out.println(leet2.combinationSum3(3, 9));
-        System.out.println(leet2.combinationSum3(4, 1));
+        System.out.println(leet2.combinationSum32(3, 7));// 三个1-9的数和为7
+        leet2.res = new ArrayList<>();
+        System.out.println(leet2.combinationSum32(3, 9));
+        leet2.res = new ArrayList<>();
+        System.out.println(leet2.combinationSum32(4, 1));
+        System.out.println("---------");
+        leet2.res = new ArrayList<>();
+        System.out.println(leet2.combinationSum32(2, 18));
     }
 
     List<List<Integer>> res = new ArrayList<>();
@@ -47,16 +54,15 @@ public class LeetCode216 {
         }
 
         list.add(start);
-        k--;
-        recursion(k, n, start + 1, list);
-        k++;
+        recursion(k - 1, n, start + 1, list);
         list.remove(list.size() - 1);
 
         recursion(k, n , start + 1, list);
     }
 
     public List<List<Integer>> combinationSum32(int k, int n) {
-        recursion2(k, n, 1, new ArrayList<Integer>());
+        target = n;
+        recursion3(k, n, 1, new ArrayList<Integer>());
 
         return res;
     }
@@ -71,15 +77,27 @@ public class LeetCode216 {
             }
             return;
         }
-        if (start > 9){
+
+        for (int i = start; i < n && i <= 9; i++) {
+            list.add(i);
+            recursion2(k - 1, n, i + 1, list);
+            list.remove(list.size() - 1);
+
+        }
+    }
+
+    int target = 0;
+    public void recursion3(int k, int n, int start, List<Integer> list){
+        if (k == 0){
+            if (0 == n) {
+                res.add(new ArrayList<>(list));
+            }
             return;
         }
 
-        for (int i = start; i < n; i++) {
-            list.add(start);
-            k--;
-            recursion2(k, n, i + 1, list);
-            k++;
+        for (int i = start;  i <= 9 && i < target; i++) {
+            list.add(i);
+            recursion3(k - 1, n - i, i + 1, list);
             list.remove(list.size() - 1);
 
         }
