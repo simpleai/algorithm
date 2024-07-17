a051b46b2889a29e80d619566557ebe53981ec7b
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2024-07-17 15:29:34 +0800

    feat: git reset soft 测试

--- src/main/java/com/xiaoruiit/multi_thread/ThreeOrderPringV2.java
+++ src/main/java/com/xiaoruiit/multi_thread/ThreeOrderPringV2.java
@@ -5,6 +5,8 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
+ * 循环顺序打印A、B、C
+ *
  * @author hanxiaorui
  * @date 2024/7/17
  */
@@ -17,15 +19,15 @@ public class ThreeOrderPringV2 {
     private static final AtomicInteger currentCharIndex = new AtomicInteger(); // a:0, b:1, c:2
 
     public static void main(String[] args) throws InterruptedException {
-        Thread threadA = new Thread(new PrintChar('A'));
-        Thread threadB = new Thread(new PrintChar('B'));
-        Thread threadC = new Thread(new PrintChar('C'));
+        Thread threadA = new Thread(new PrintChar('A', 0));
+        Thread threadB = new Thread(new PrintChar('B', 1));
+        Thread threadC = new Thread(new PrintChar('C', 2));
 
         threadA.start();
         threadB.start();
         threadC.start();
 
-        threadA.join();
+        threadA.join();// 加入主线程，防止主线程结束后，子线程无法在主线程中显示打印结果
         threadB.join();
         threadC.join();
     }
@@ -36,9 +38,9 @@ public class ThreeOrderPringV2 {
 
         int index;
 
-        public PrintChar(char str){
+        public PrintChar(char str, int index){
             this.c = str;
-            this.index = c == 'A' ? 0 : c == 'B' ? 1 : 2;
+            this.index = index;
         }
         @Override
         public void run() {
